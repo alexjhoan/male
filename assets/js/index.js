c9641238e87362f49ff97a0b7c7097e5032831ad
alexjhoan <alexjhoan.25@gmail.com>
2021-07-13 21:19:51 -0400

    change porcentage

--- assets/js/index.js
+++ assets/js/index.js
@@ -449,7 +449,7 @@ if (screen.width > 768){
 }
 
 $(document).ready(function () {
-  autoProgress('2020/12/01', '2023/04/01', 'progressBar', 'IdImpPercentage')
+  autoProgress('2020/05/15', '2023/04/01', 'progressBar', 'IdImpPercentage')
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
